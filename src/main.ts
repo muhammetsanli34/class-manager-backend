8cfd3029ca7201450f9c462287727191c7457d07
Muhammet Emin Şanlı <muhammetsanli34@gmail.com>
2024-07-20 14:12:43 +0300

    add swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Class Management System')
+    .setDescription('The Class Management System API description')
+    .setVersion('1.0')
+    .addTag('class-manager')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
